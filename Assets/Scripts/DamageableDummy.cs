1e9252adb33366ac268ff3037ef9242f7a8092c9
senqa <semen7498@icloud.com>
2026-02-03 16:59:47 +1000

    Add game manager

--- Assets/Scripts/DamageableDummy.cs
+++ Assets/Scripts/DamageableDummy.cs
@@ -4,12 +4,20 @@ using VR.Weapons;
 public class DamageableDummy : MonoBehaviour, IDamageable
 {
     public float health = 100f;
+    public int pointsOnKill = 10; // Points awarded when destroyed
+
     public void ApplyHit(HitInfo info)
     {
         health -= info.damage;
         Debug.Log($"Hit {name} for {info.damage}. Health now {health}");
         if (health <= 0f)
         {
+            // If the GameManager exists, add score
+            if (GameManager.instance != null)
+            {
+                GameManager.instance.AddScore(pointsOnKill);
+            }
+
             Debug.Log($"{name} destroyed");
             Destroy(gameObject);
         }
